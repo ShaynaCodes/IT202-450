2da70fd02d0a553e4969c16024c50b6aaa5b3e4e
sf363 <sf363@njit.edu>
2020-06-05 23:55:24 -0400

    made sessions folder

--- public_html/Sessions/register.php
+++ public_html/Sessions/register.php
@@ -5,46 +5,41 @@
 	<label for="p">Password
 	<input type="password" id="p" name="password" />
 	</label>
-	<input type ="submit" name="login" value="Login"/> 
+	<label for = "cp">Confirm Password
+	<input type="password" id="cp" name="cpassword"/>
+	</label>
+	<input type ="submit" name="register" value="Register"/> 
 </form>
 
 <?php
-if(isset($_POST["login"])){
-if(isset($_POST["password"]) && isset($_POST["email"])){
+if(isset($_POST["register"])){
+if(isset($_POST["password"]) && isset($_POST["cpassword"]) && isset($_POST["email"])){
 		$password = $_POST["password"];
+		$cpassword = $_POST["cpassword"];
 		$email = $_POST["email"];
-		require("config.php");
+		if($password == $cpassword){
+			//echo "<div>Password Match</div>";
+			require("config.php");
 			$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 			try{
 				$db = new PDO($connection_string, $dbuser, $dbpass);
-				$stmt = $db->prepare("SELECT * FROM User where email = :email LIMIT 1");
+				$stmt = $db->prepare("INSERT INTO Users (email, password) VALUES(:email, :password)");
 				$stmt->execute(array(
 					":email" => $email,
+					":password" => $password
 					));
 					$e = $stmt->errorInfo();
 					if($e[0] != "00000"){
 						echo var_export($e, true);
 					}
 					else{
-						$result = $stmt->fetch(PDO::FETCH_ASSOC);
-						if($result){
-							$rpassword = $result["password"];
-							if($password_verify($password, $rpassword)){
-								echo "<div>Passwords Match! You are logged in!</div>";
-							}
-							else{
-								echo "<div>Invalid Password</div>";
-							}
-						}
-						else{
-							echo "<div>Invalid User</div>";
-						}
+						echo "<div>Successfully registered!</div>";
 					}
 				}
-
 			catch (Exception $e){
 				echo $e->getMessage();
 			}
+		}
 		else{
 			echo "<div>Password Do Not Match</div>";
 		}
