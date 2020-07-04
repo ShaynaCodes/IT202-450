bf4b25b5de6e79a19e87d118ffa89caa2918d5b9
sf363 <sf363@njit.edu>
2020-07-03 22:07:03 -0400

    milestone 2 project

--- public_html/Project/Question.php
+++ public_html/Project/Question.php
@@ -37,13 +37,13 @@ if(isset($_POST["survey"])){
 			$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 			try{
 				$db = new PDO($connection_string, $dbuser, $dbpass);
-				$stmt = $db->prepare("INSERT INTO Survey (question) VALUES (:question)");
+				$stmt = $db->prepare("INSERT INTO Survey (question, Option1, Option2, Option3, Option4) VALUES (:question, :answer1, :answer2, :answer3, :answer4)");
 				$r = $stmt->execute(array(
 					":question"=> $question
-					//":answer1"=> $Option1
-					//":answer2"=> $Qption2
-					//":answer3"=> $Option3
-					//":answer4"=> $Option4
+					":answer1"=> $Option1
+					":answer2"=> $Qption2
+					":answer3"=> $Option3
+					":answer4"=> $Option4
 				));
 				echo var_export($stmt->errorInfo(), true);
 				echo var_export($r, true);
