5d6f2a07988f9d882cf8eca635d6537971223642
sf363 <sf363@njit.edu>
2020-07-30 17:47:03 -0400

    fix

--- public_html/final/partials/header.partial.php
+++ public_html/final/partials/header.partial.php
@@ -1,5 +1,5 @@
 <?php
-require_once (__DIR__."/../includes/common.inc.php");
+//require_once (__DIR__."/../includes/common.inc.php");
 $logged_in = Common::is_logged_in(false);
 ?>
 <!-- Bootstrap 4 CSS only -->
