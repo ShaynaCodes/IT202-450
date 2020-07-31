a2aa284bb73f41054b8b1d6017742135f323fee8
sf363 <sf363@njit.edu>
2020-07-31 18:02:21 -0400

    fixed header

--- public_html/project/partials/header.partial.php
+++ public_html/project/partials/header.partial.php
@@ -14,39 +14,18 @@ $logged_in = Common::is_logged_in(false);
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-            <a class="nav-link" href="<?php echo Common::url_for("index");?>">Home</a>
+            <a class="nav-link" href="<?php echo Common::url_for("surveys");?>">Home</a>
         </li>
         <?php if($logged_in):?>
         <li class="nav-item">
-            <a class="nav-link" href="<?php echo Common::url_for("dashboard");?>">Dashboard</a>
+            <a class="nav-link" href="<?php echo Common::url_for("survey");?>">Survey</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="<?php echo Common::url_for("game");?>">Game</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="<?php echo Common::url_for("rankings");?>">Rankings</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="<?php echo Common::url_for("shop");?>">Shop</a>
+            <a class="nav-link" href="<?php echo Common::url_for("results");?>">Results</a>
         </li>
         <?php if (Common::has_role("Admin")):?>
         <li class="nav-item">
             <a class="nav-link" href="<?php echo Common::url_for("create_questionnaire");?>">Create Questionnaire</a>
-        </li>
-        <?php endif;?>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="compDropdown" role="button"
-               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Competitions
-            </a>
-            <div class="dropdown-menu" aria-labelledby="compDropdown">
-                <a class="dropdown-item" href="<?php echo Common::url_for("create_competition");?>">Create</a>
-                <a class="dropdown-item" href="<?php echo Common::url_for("competitions");?>">Available</a>
-                <a class="dropdown-item" href="<?php echo Common::url_for("competitions");?>?status=registered">Registered</a>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="<?php echo Common::url_for("surveys");?>">Surveys</a>
         </li>
         <?php endif; ?>
         <?php if(!$logged_in):?>
@@ -62,11 +41,6 @@ $logged_in = Common::is_logged_in(false);
             </li>
         <?php endif; ?>
     </ul>
-    <?php if($logged_in):?>
-    <span class="navbar-text">
-      Balance: <?php echo Common::get($_SESSION["user"], "points", 0);?>
-    </span>
-    <?php endif;?>
 </nav>
 <div id="messages">
     <?php $flash_messages = Common::getFlashMessages();?>
