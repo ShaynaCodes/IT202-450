6022b96fa5c230b0ce9853bf989facd7bd636471
sf363 <sf363@njit.edu>
2020-08-02 21:16:17 -0400

    update

--- public_html/project/edit.php
+++ public_html/project/edit.php
@@ -1,5 +1,5 @@
 <?php
-require("config.php");
+/*require("config.php");
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 $db = new PDO($connection_string, $dbuser, $dbpass);
 $surveyId = -1;
@@ -18,7 +18,7 @@ if(isset($_GET["surveyId"])){
 }
 else{
     echo "No surveyId provided in url, don't forget this or sample won't work.";
-}
+}*/
 ?>
 
 <form method="POST">
@@ -36,17 +36,17 @@ if (Common::get($_POST, "submit", false)){
     $email = Common::get($_POST, "email", false);
     $password = Common::get($_POST, "password", false);
     if(!empty($email) && !empty($password)){
-		$result = DBH::update($email, $password);
-		 echo var_export($result, true);
+        $result = DBH::update($email, $password);
+        echo var_export($result, true);
         if(Common::get($result, "status", 400) == 200){
             //Note to self: Intentionally didn't add tank creation here
             //keeping it in login where it is (creates a new tank only if user has no tanks)
             //it fulfills the purpose there
             Common::flash("Successfully registered, please login", "success");
             $data = Common::get($result, "data", []);
-            $surveyId = Common::get($data,"user_id", -1);
-            if($surveyId > -1) {
-                $result = DBH::check_survey_status($surveyId, 10, -1, "earned", "Welcome");
+            $id = Common::get($data,"user_id", -1);
+            if($id > -1) {
+                $result = DBH::check_survey_status($id, 10, -1, "earned", "Welcome");
                 if(Common::get($result, "status", 400) == 200){
                     Common::flash("You can now create and participate in Surveys!", "success");
                 }
