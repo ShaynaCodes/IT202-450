796b0204ac182c4a841c9327ef4ccb951c92a125
sf363 <sf363@njit.edu>
2020-08-03 11:29:08 -0400

    edit

--- public_html/project/edit.php
+++ public_html/project/edit.php
@@ -6,40 +6,44 @@ include_once(__DIR__."/partials/header.partial.php");
         <form method="POST">
             <div>
                 <label for="email">Email</label>
-                <input type="email" id="email" name="email" required/>
+                <input type="email" id="email" name="email" />
             </div>
             <div>
                 <label for="password">Password</label>
                 <input type="password" id="password" name="password" required min="3"/>
             </div>
-            <input type="submit" name="submit" value="update"/>
+            <input type="submit" name="submit" value="Update"/>
         </form>
     </div>
 <?php
-if (Common::get($_POST, "submit", false)){
+iif (Common::get($_POST, "submit", false)){
     $email = Common::get($_POST, "email", false);
-    $password = Common::get($_POST, "password", false)
+    $password = Common::get($_POST, "password", false);
     if(!empty($email) && !empty($password)){
-        $result = DBH::update($email, $password);
+        $result = DBH::login($email, $password);
         echo var_export($result, true);
         if(Common::get($result, "status", 400) == 200){
-            //Note to self: Intentionally didn't add tank creation here
-            //keeping it in login where it is (creates a new tank only if user has no tanks)
-            //it fulfills the purpose there
-            Common::flash("Successfully Updated, please login", "success");
-            $data = Common::get($result, "data", []);
-            $id = Common::get($data,"user_id", -1);
-            /*if($id > -1) {
-                $result = DBH::changePoints($id, 10, -1, "earned", "Welcome bonus");
-                if(Common::get($result, "status", 400) == 200){
-                    Common::flash("Here's 10 free points for the shop to start you off!", "success");
-                }
-            }*/
+            $_SESSION["user"] = Common::get($result, "data", NULL);
+
+            //fetch system user id and put it in session to reduce DB calls to fetch it when we need
+            //to generate points from activity on the app
+            $result = DBH::get_system_user_id();
+            $result = Common::get($result, "data", false);
+            if($result) {
+                $_SESSION["system_id"] = Common::get($result, "id", -1);
+                error_log("Got system_id " . $_SESSION["system_id"]);
+            }
+
+            die(header("Location: " . Common::url_for("dashboard")));
+        }
+        else{
+            Common::flash(Common::get($result, "message", "Error logging in"));
             die(header("Location: " . Common::url_for("login")));
         }
     }
     else{
         Common::flash("Email and password must not be empty", "warning");
-        die(header("Location: register.php"));
+        die(header("Location: " . Common::url_for("login")));
     }
 }
+?>
