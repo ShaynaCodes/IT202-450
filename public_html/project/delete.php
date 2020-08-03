f46124c9c3fcca6164b1caa3dad96c07f1e63ea0
sf363 <sf363@njit.edu>
2020-08-03 12:25:49 -0400

    delete

--- public_html/project/delete.php
+++ public_html/project/delete.php
@@ -1,18 +1,18 @@
 <?php
-if (isset($_GET["thingId"]) && !empty($_GET["thingId"])){
-    if(is_numeric($_GET["thingId"])){
-        $thingId = (int)$_GET["thingId"];
-        $query = file_get_contents(__DIR__ . "/sql/queries/DELETE_ONE_TABLE_THINGS.sql");
+if (isset($_GET["id"]) && !empty($_GET["id"])){
+    if(is_numeric($_GET["id"])){
+        $id = (int)$_GET["id"];
+        $query = file_get_contents(__DIR__ . "/sql/queries/DELETE_ONE_TABLE_QUESTION.sql");
         if(isset($query) && !empty($query)) {
             require("common.inc.php");
             $stmt = getDB()->prepare($query);
-            $stmt->execute([":id"=>$thingId]);
+            $stmt->execute([":id"=>$id]);
             $e = $stmt->errorInfo();
             if($e[0] == "00000"){
                 //we're just going to redirect back to the list
                 //it'll reflect the delete on reload
                 //also wrap it in a die() to prevent the script from any continued execution
-                die(header("Location: list.php"));
+                die(header("Location: surveys.php"));
             }
             else{
                 echo var_export($e, true);
